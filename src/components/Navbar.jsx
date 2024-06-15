83f3269eabd0190484e4107de761aaccd31f21ae
Daniel Bauer <dan717@gmx.de>
2024-06-15 16:30:03 +0200

    Added hero section with GSAP animations and responsive video
    Implemented hero section with full-width and flexible layout
    Added title and styled with Tailwind CSS
    Integrated GSAP for smooth fade-in animation of the title
    Embedded responsive video, switching between different sources based on screen size
    Added call-to-action button with GSAP animation for smooth transition

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,11 +5,11 @@ import { navLists } from "../constants";
 const Navbar = () => {
   return (
     // Hauptcontainer des Navigationsbalkens
-    <header className="w-full py-5 sm:px-10 px-5 flex justify-between items-center">
+    <header className="flex w-full items-center justify-between p-5 sm:px-10">
       {/* Navigationscontainer, passt sich der maximalen Bildschirmbreite an */}
-      <nav className="flex w-full screen-max-width">
+      <nav className="screen-max-width flex w-full">
         <img src={appleImg} alt="Apple" width={14} height={18} />
-        {/* Container für Navigationslinks, nur sichtbar auf größeren Bildschirmen*/}
+        {/* Container für Navigationslinks, nur sichtbar auf größeren Bildschirmen */}
         <div className="flex flex-1 justify-center max-sm:hidden">
           {navLists.map((nav, index) => (
             // Navigationslinks mit Hover-Effekt
@@ -24,14 +24,14 @@ const Navbar = () => {
             */
             <div
               key={nav}
-              className="px-5 text-sm cursor-pointer text-gray hover:text-white transition-all"
+              className="cursor-pointer px-5 text-sm text-gray transition-all hover:text-white"
             >
               {nav}
             </div>
           ))}
         </div>
 
-        <div className="flex items-baseline gap-7 max-sm:justify-end max-sm:flex-1">
+        <div className="flex items-baseline gap-7 max-sm:flex-1 max-sm:justify-end">
           <img src={searchImg} alt="Search" width={18} height={18} />
 
           <img src={bagImg} alt="Bag" width={18} height={18} />
